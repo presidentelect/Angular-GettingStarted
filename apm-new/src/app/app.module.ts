dd58d328eb47d6b8ee91b7031d434eaf8f319c72
Michael Hansen <m.hansen@pobox.com>
2022-07-07 14:37:07 -0400

    working on the exercises

--- apm-new/src/app/app.module.ts
+++ apm-new/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
